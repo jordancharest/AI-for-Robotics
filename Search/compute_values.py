1743269641a98ff24a1a7b6580b8eb4feb199118
Jordan <jordancharest3@gmail.com>
2018-03-19 16:35:53 -0400

    Added clarifying comments

--- Search/compute_values.py
+++ Search/compute_values.py
@@ -16,19 +16,25 @@ def compute_value(grid,goal,cost):
         
         for x in range(len(grid)):
             for y in range(len(grid[0])):
+                # only change the goal state once
                 if goal[0] == x and goal[1] == y:
                     if value[x][y] > 0:
                         value[x][y] = 0
                         change = True
-                        
+                
+                # if the current grid cell is not an obstacle
                 elif grid[x][y] == 0:
+                    
+                    # check all four directions
                     for a in range(len(delta)):
                         x2 = x + delta[a][0]
                         y2 = y + delta[a][1]
                         
+                        # if the grid cell is valid, compute the hypothetical value of that grid cell
                         if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]) and grid[x2][y2] == 0:
                             v2 = value[x2][y2] + cost
                             
+                            #only update if its less than the current value
                             if v2 < value[x][y]:
                                 change = True
                                 value[x][y] = v2
@@ -56,4 +62,4 @@ delta = [[-1, 0 ], # go up
 
 delta_name = ['^', '<', 'v', '>']
 
-compute_value(grid, goal, cost)
+costs = compute_value(grid, goal, cost)
