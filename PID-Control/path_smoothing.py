df506381f7141a78e93490cca3dd7e3c22aa2e00
Jordan <jordancharest3@gmail.com>
2018-03-22 23:26:21 -0400

    Implemented cyclic path smoothing and path smoothing with fixed points

--- PID-Control/path_smoothing.py
+++ PID-Control/path_smoothing.py
@@ -7,11 +7,16 @@ def printpaths(path,newpath):
         print ('['+ ', '.join('%.3f'%x for x in old) + \
                '] -> ['+ ', '.join('%.3f'%x for x in new) +']')
 
-
-def smooth(path, weight_data = 0.5, weight_smooth = 0.1, tolerance = 0.000001):
+    
+def smooth_w_fixed_points(path, fixed, weight_data = 0.1, weight_smooth = 0.1, tolerance = 0.000001, cyclic = False):
 
     # Make a deep copy of path into newpath
     new_path = deepcopy(path)
+    
+    if (cyclic):
+        iter_range = range(len(path))
+    else:
+        iter_range = range(1, len(path)-1)  # don't smooth the endpoints
     
     change = tolerance
     
@@ -19,13 +24,54 @@ def smooth(path, weight_data = 0.5, weight_smooth = 0.1, tolerance = 0.000001):
         change = 0.0
         
         # iterate over each coordinate individually (treat x and y as independent)
-        for i in range(1, len(path)-1):     # don't smooth the endpoints
+        
+        for i in iter_range:
+            if not fixed[i]:
+                for j in range(len(path[0])):
+                    start = new_path[i][j]  # cache to compute how much we have changed (break after convergence)
+                    
+                    # gradient descent update
+                    new_path[i][j] += weight_smooth * (new_path[(i-1)%len(path)][j] + new_path[(i+1)%len(path)][j] - \
+                                        2.0 * new_path[i][j]) + \
+                                        (weight_smooth / 2.0) * (2.0 * new_path[(i-1)%len(path)][j] - \
+                                        new_path[(i-2)%len(path)][j] - new_path[i][j]) + \
+                                        (weight_smooth / 2.0) * (2.0 * new_path[(i+1)%len(path)][j] - \
+                                        new_path[(i+2)%len(path)][j] - new_path[i][j])
+                    
+                    # mod len(path) above will be irrelevant in the non-cyclic case
+                    
+                    change = abs(new_path[i][j] - start)                                
+                                 
+    return new_path
+
+
+
+def smooth(path, weight_data = 0.1, weight_smooth = 0.1, tolerance = 0.000001, cyclic = False):
+
+    # Make a deep copy of path into newpath
+    new_path = deepcopy(path)
+    
+    if (cyclic):
+        iter_range = range(len(path))
+    else:
+        iter_range = range(1, len(path)-1)  # don't smooth the endpoints
+    
+    change = tolerance
+    
+    while change >= tolerance:
+        change = 0.0
+        
+        # iterate over each coordinate individually (treat x and y as independent)
+        
+        for i in iter_range:     
             for j in range(len(path[0])):
                 start = new_path[i][j]  # cache to compute how much we have changed (break after convergence)
                 
                 # gradient descent update
                 new_path[i][j] += weight_data * (path[i][j] - new_path[i][j]) +  \
-                                    weight_smooth * (new_path[i+1][j] + new_path[i-1][j] - 2*new_path[i][j])
+                                    weight_smooth * (new_path[(i+1)%len(path)][j] + new_path[(i-1)%len(path)][j] - 2*new_path[i][j])
+                
+                # mod len(path) above will be irrelevant in the non-cyclic case
                 
                 change = abs(new_path[i][j] - start)                                
                                  
@@ -35,6 +81,7 @@ def smooth(path, weight_data = 0.5, weight_smooth = 0.1, tolerance = 0.000001):
 ## MAIN =======================================================================
 if __name__ == "__main__":
     
+    print('Smoothing')
     path = [[0, 0],
             [0, 1],
             [0, 2],
@@ -45,3 +92,63 @@ if __name__ == "__main__":
             [4, 3],
             [4, 4]]
     printpaths(path,smooth(path))
+    
+    # Test smoothing with fixed points
+    print('\nSmoothing with fixed points')
+    path = [[0, 0],
+            [0, 1],
+            [0, 2],
+            [1, 2],
+            [2, 2],
+            [3, 2],
+            [4, 2],
+            [4, 3],
+            [4, 4]]
+    fixed = [0,0,1,0,0,0,1,0,0]
+    printpaths(path,smooth_w_fixed_points(path, fixed))
+    
+    # Test cyclic smoothing
+    print('\nCyclic smoothing')
+    path=[[0, 0], 
+          [1, 0],
+          [2, 0],
+          [3, 0],
+          [4, 0],
+          [5, 0],
+          [6, 0],
+          [6, 1],
+          [6, 2],
+          [6, 3],
+          [5, 3],
+          [4, 3],
+          [3, 3],
+          [2, 3],
+          [1, 3],
+          [0, 3],
+          [0, 2],
+          [0, 1]]
+    printpaths(path,smooth(path, cyclic=True))
+    
+    
+    # Test cyclic smoothing with fixed points
+    print('\nCyclic smoothing with fixed points')
+    path=[[0, 0], 
+          [1, 0],
+          [2, 0],
+          [3, 0],
+          [4, 0],
+          [5, 0],
+          [6, 0],
+          [6, 1],
+          [6, 2],
+          [6, 3],
+          [5, 3],
+          [4, 3],
+          [3, 3],
+          [2, 3],
+          [1, 3],
+          [0, 3],
+          [0, 2],
+          [0, 1]]
+    fixed = [1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0]
+    printpaths(path,smooth_w_fixed_points(path, fixed, cyclic=True))
